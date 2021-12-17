d4b06e24dc2cbb1dbb01f4477dcfbafc06202fb6
ShoichiroKitano <ohmybigcatkotarou@gmail.com>
2021-12-18 07:25:14 +0900

    go

--- bash_profile_for_ubuntu.sh
+++ bash_profile_for_ubuntu.sh
@@ -4,3 +4,7 @@ PS1='\W$(__git_ps1)$ '
 
 export PATH="$HOME/.anyenv/bin:$PATH"
 eval "$(anyenv init -)"
+
+export GOPATH="$HOME/project/gomodule"
+export GO111MODULE=on
+export GOENV_DISABLE_GOPATH=1
